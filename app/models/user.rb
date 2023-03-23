008e9737b8a0641fc51657d8b0c1b4907b8d0b7e
barsu5489 <ebarsulai@gmail.com>
2023-03-24 00:57:23 +0300

    password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :recipes
     has_secure_password
-    validates :username, presence: true, uniquness: true 
-    validates :instructions, presence: true, length: {minimum: 50}
+    validates :username, presence: true, uniqueness: true 
+
 end
