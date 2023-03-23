b1f8ceb6d1acb666de2d3c097b46a313102291a8
barsu5489 <ebarsulai@gmail.com>
2023-03-23 23:42:14 +0300

    Recipie validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :recipes
     has_secure_password
     validates :username, presence: true, uniquness: true 
+    validates :instructions, presence: true, length: {minimum: 50}
 end
